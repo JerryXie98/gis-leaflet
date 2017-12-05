d2686358aea575349eae7ae1954e61b8ef11bd9b
Leo Qiu <leo@fullstacklabs.co>
2017-12-05 09:47:18 -0800

    update READme

---
+++ README.md
@@ -0,0 +1,7 @@
+# react leaflet
+[Live Demo](https://react-leaflet.stackblitz.io)
+[Live Edit](https://stackblitz.com/edit/react-leaflet)
+
+## About
+
+This project uses [React](https://reactjs.org/), [Leaflet](http://leafletjs.com/), and [esri-leaflet](https://esri.github.io/esri-leaflet/). It is a simple demo app using GeoJson type data. User can pan, zoom, and insert Markers on the map. GeoJson file can also be loaded to update the marker.
--- src/App.js
+++ src/App.js
@@ -1,84 +1,3 @@
-// import React, {Component} from 'react';
-// import './App.css';
-// import {Map, Marker, Popup, TileLayer, CircleMarker} from 'react-leaflet';
-//
-// class App extends Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {currentZoomLevel: 13};
-//   }
-//
-//   componentDidMount() {
-//     const leafletMap = this.leafletMap.leafletElement;
-//     console.log(this.leafletMap);
-//     leafletMap.on('zoomend', () => {
-//       const updatedZoomLevel = leafletMap.getZoom();
-//       this.handleZoomLevelChange(updatedZoomLevel);
-//     });
-//   }
-//
-//   handleZoomLevelChange(newZoomLevel) {
-//     this.setState({currentZoomLevel: newZoomLevel});
-//   }
-//
-//   handleUpPanClick() {
-//     const leafletMap = this.leafletMap.leafletElement;
-//     leafletMap.panBy([0, -100]);
-//     window.console.log('Panning up');
-//   }
-//
-//   render() {
-//     window.console.log(
-//       'this.state.currentZoomLevel ->',
-//       this.state.currentZoomLevel
-//     );
-//     const position = [51.505, -0.09];
-//     const position2 = [51.509, -0.09];
-//     return (
-//       <div>
-//         <Map
-//           center={position}
-//           zoom={this.state.currentZoomLevel}
-//           style={{width: '100%', height: '500px'}}
-//           ref={m => {
-//             this.leafletMap = m;
-//           }}
-//         >
-//           <TileLayer
-//             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-//             attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
-//           />
-//           <Marker position={position}>
-//             <Popup>
-//               <span>
-//                 A pretty CSS3 popup.<br />Easily customizable.
-//               </span>
-//             </Popup>
-//           </Marker>
-//           <Marker position={position2}>
-//             <Popup>
-//               <span>
-//                 A pretty CSS3 popup.<br />Easily customizable.
-//               </span>
-//             </Popup>
-//           </Marker>
-//           <CircleMarker center={[51.51, -0.12]} color="red" radius={20}>
-//             <Popup>
-//               <span>
-//                 A pretty CSS3 popup.<br />Easily customizable.
-//               </span>
-//             </Popup>
-//           </CircleMarker>
-//         </Map>
-//         <span>hello world</span>
-//         <button onClick={() => this.handleUpPanClick()}>Click me </button>
-//       </div>
-//     );
-//   }
-// }
-//
-// export default App;
-
 import React, {Component} from 'react';
 import './App.css';
 import L from 'leaflet';
@@ -87,27 +6,30 @@ import esri from 'esri-leaflet';
 class App extends Component {
   constructor(props) {
     super(props);
+    //the map is handled by the state, so if you want to change the map's property
+    //like zoom or pan, you can just call it by this.state.map.zoomIn(), shown
+    // in line 22
     this.state = {currentZoomLevel: 13, map: null, tileLayer: null};
   }
 
   componentDidMount() {
-    this.init();
+    this.initMap();
   }
 
+  //Event Handler for changing the zoom.
   async changeZoom() {
-    await this.setState({currentZoomLevel: 1});
-    console.log(this._mapNode);
-    L.marker([51.505, -0.092]).addTo(this.state.map);
-    this.state.map.zoomIn();
+    var map = L.marker([51.505, -0.092]).addTo(this.state.map);
+    map = this.state.map.zoomIn();
+    await this.setState({map});
   }
 
-  init() {
+  //Loading the Map, this only gets called once.
+  initMap() {
     const position = [51.505, -0.09];
     const map = L.map(this._mapNode).setView(
       position,
       this.state.currentZoomLevel
     );
-
     const tileLayer = esri.basemapLayer('Topographic').addTo(map);
 
     this.setState({map, tileLayer});
@@ -118,10 +40,13 @@ class App extends Component {
   }
 
   render() {
-    window.console.log(
-      'this.state.currentZoomLevel ->',
-      this.state.currentZoomLevel
-    );
+    if (this.state.map) {
+      window.console.log(
+        'this.state.currentZoomLevel ->',
+        this.state.map._zoom
+      );
+    }
+
     return (
       <div>
         <div
